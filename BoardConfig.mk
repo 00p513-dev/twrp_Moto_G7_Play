919506c5744922804ec37f14babdcc8f58a339d8
Eva Kane <betaxab@gmail.com>
2019-03-24 17:22:31 +0800

    exclude twrpapp
    Signed-off-by: Eva Kane <betaxab@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_USE_TOOLBOX := true
+TW_EXCLUDE_TWRPAPP := true
 # MTP will not work until we update it to support ffs
 TW_EXCLUDE_MTP := true
 
